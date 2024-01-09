a1b56dfe9f07a9b356e654dea3f7ae8eb2679dae
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-09 19:28:48 +0000

    new project

---
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script to initialize a new C# project
+
+# Create a new directory for the project
+mkdir -p 0-new_project
+
+# Navigate to the directory
+cd 0-new_project
+
+# Initialize a new C# console application
+dotnet new console
