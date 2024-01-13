89d4e89d18e11de5b32470ca19e3f700f2da4144
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-13 14:44:29 +0000

    6

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string str = "Holberton School"; // Example string
+        string str = "Holberton School";
         Console.WriteLine(str + str + str);
         Console.WriteLine(str.Substring(0, 9));
     }
