fac4b533ef4d430633c5cd021060033d7111c929
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-13 17:32:18 +0000

    7

--- csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -3,10 +3,9 @@ using System;
 class Character
 {
     public static bool IsLower(char c)
-{
-    return c >= 'a' && c <= 'z';
-}
-
+    {
+        return c >= 'a' && c <= 'z';
+    }
 }
 
 class Program
@@ -19,9 +18,22 @@ class Program
         for (i = 0; i < letters.Length; i++)
         {
             if (Character.IsLower(letters[i]))
+            {
                 Console.WriteLine("{0} is lowercase", letters[i]);
-            else
+            }
+            else if (Character.IsUpper(letters[i]))
+            {
                 Console.WriteLine("{0} is uppercase", letters[i]);
+            }
+            else
+            {
+                Console.WriteLine("{0} is neither lowercase nor uppercase", letters[i]);
+            }
         }
+    }
+
+    public static bool IsUpper(char c)
+    {
+        return c >= 'A' && c <= 'Z';
     }
 }
