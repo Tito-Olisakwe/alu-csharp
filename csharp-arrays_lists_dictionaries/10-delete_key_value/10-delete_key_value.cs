7f3e22bf2da79f6286b97828f8cc2be30fb68dad
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-20 23:21:55 +0000

    10

--- csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
+﻿using System.Collections;
 
-public class Dictionary
-{
-    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
-    {
-        // Check if the dictionary contains the key and remove it
-        if (myDict.ContainsKey(key))
-        {
-            myDict.Remove(key);
+
+class Dictionary{
+    public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key){
+        Dictionary<string, string> tempDict = myDict;
+        if(tempDict.ContainsKey(key)){
+            tempDict.Remove(key);
         }
 
-        // Return the original dictionary
-        return myDict;
+        return tempDict;
+
     }
 }
