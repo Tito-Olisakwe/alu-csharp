a663ed11ce0f30cd41413b60d43d8c6f1106c9a4
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-20 22:03:30 +0000

    3

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections;
 
-class MyStack
-{
-    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
-    {
-        string item = "";
+class Array{
 
-        Console.WriteLine("Number of items: {0}", aStack.Count);
-        if (aStack.Count == 0)
-        {
-            item = "Stack is empty";
-        }
-        else
-        {
-            item = "Top item: " + aStack.Peek();
-        }
-        Console.WriteLine(item);
-        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
-        while (aStack.Contains(search))
-        {
-            aStack.Pop();
-        }
-        aStack.Push(newItem);
-        return aStack;
+    public static void Reverse(int[] array){
+     
+       if(array != null){
+            int size = array.Length;
+        if(size > 0 && size != null){
+
+            for(int i = size - 1; i >= 0; i--){
+                Console.Write(array[i] + " ");
+                }
+                    Console.WriteLine();
+             }else{
+                Console.WriteLine();
+             }
+       }else{
+        Console.WriteLine();
+       }
+       
+     
     }
 }
