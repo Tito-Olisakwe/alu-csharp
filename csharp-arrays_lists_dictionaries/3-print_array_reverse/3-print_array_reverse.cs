f37a5763fd5422b19c2ea3f9e3e5fa0d7b738f1d
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-20 22:10:39 +0000

    3

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -1,24 +1,20 @@
-using System;
+﻿using System;
+using System.Text;
 
-class Array{
-
-    public static void Reverse(int[] array){
-     
-       if(array != null){
-            int size = array.Length;
-        if(size > 0 && size != null){
-
-            for(int i = size - 1; i >= 0; i--){
-                Console.Write(array[i] + " ");
-                }
-                    Console.WriteLine();
-             }else{
-                Console.WriteLine();
-             }
-       }else{
-        Console.WriteLine();
-       }
-       
-     
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        var output = new StringBuilder();
+        if (array != null)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                output.Append(array[i]);
+                if (i != 0)
+                    output.Append(" ");
+            }
+        }
+        Console.WriteLine(output);
     }
 }
