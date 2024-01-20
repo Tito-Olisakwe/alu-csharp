525b294a2a5eb7c527c7e4da939cf92aa34ca303
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-20 21:49:59 +0000

    3

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -4,13 +4,7 @@ public class Array
 {
     public static void Reverse(int[] array)
     {
-        // No null check. Directly accessing array.Length will trigger a warning if array is null
-        if (array.Length == 0)
-        {
-            Console.WriteLine();
-            return;
-        }
-
+        // No null checks; proceed directly to processing the array
         for (int i = array.Length - 1; i >= 0; i--)
         {
             Console.Write(array[i] + (i > 0 ? " " : "\n"));
