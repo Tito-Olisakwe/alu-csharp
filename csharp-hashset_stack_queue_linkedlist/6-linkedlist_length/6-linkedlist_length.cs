2e9ada6c80b70ea4921b4e6fcd7dccee34bf902a
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-21 00:28:40 +0000

    6

--- csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -5,6 +5,12 @@ public class LList
 {
     public static int Length(LinkedList<int> myLList)
     {
+        // Check if the LinkedList is null
+        if (myLList == null)
+        {
+            return 0; // or throw an exception, based on your use case
+        }
+
         int count = 0;
         LinkedListNode<int> current = myLList.First;
 
