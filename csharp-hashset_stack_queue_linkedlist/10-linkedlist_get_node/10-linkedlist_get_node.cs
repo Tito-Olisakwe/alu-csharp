e3ac063b84e14d02d3b7fefcac4508dbf13572a1
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-21 01:13:57 +0000

    10

--- csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -32,23 +32,4 @@ public class LList
         return 0;
     }
 
-    static void Main()
-    {
-        LinkedList<int> myLinkedList = new LinkedList<int>();
-
-        // Adding some elements to the LinkedList
-        myLinkedList.AddLast(91);
-        myLinkedList.AddLast(-22);
-        myLinkedList.AddLast(13);
-        myLinkedList.AddLast(14);
-        myLinkedList.AddLast(-54);
-        myLinkedList.AddLast(66);
-        myLinkedList.AddLast(37);
-        myLinkedList.AddLast(-8);
-
-        // Trying to get the value of node at different positions
-        Console.WriteLine("Value at index 5: " + GetNode(myLinkedList, 5));
-        Console.WriteLine("Value at index 0: " + GetNode(myLinkedList, 0));
-        Console.WriteLine("Value at index 10 (non-existent): " + GetNode(myLinkedList, 10));
-    }
 }
