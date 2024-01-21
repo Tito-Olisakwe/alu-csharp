fb3b63eb8cb08cfcc5e4c1c6b633579e0bfe98c5
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-21 11:54:01 +0000

    12

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -1,31 +1,17 @@
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-public class LList
-{
-    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
-    {
-        // Handle the case where the list is null
-        if (myLList == null)
-        {
-            myLList = new LinkedList<int>();
+class LList{
+
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n){
+            LinkedListNode<int> current;
+            current = myLList.First;
+            while(current != null){
+                if(current.Value >= n){
+                    myLList.AddBefore(current, n);
+                    return current.Previous;
+                }
+              current = current.Next;
+            }
+            return current;
         }
-
-        LinkedListNode<int> current = myLList.First;
-
-        // If the list is empty or the new value is smaller than the first value, add to the front
-        if (current == null || current.Value >= n)
-        {
-            return myLList.AddFirst(n);
-        }
-
-        // Iterate through the list to find the correct position
-        while (current.Next != null && current.Next.Value < n)
-        {
-            current = current.Next;
-        }
-
-        // Insert the new node
-        return myLList.AddAfter(current, n);
     }
-}
