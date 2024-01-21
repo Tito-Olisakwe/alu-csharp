1911cc39897a21e8b5b4b123c9da8df28c7e0ac2
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-01-21 11:55:38 +0000

    13

--- csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -1,39 +1,17 @@
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-public class LList
-{
-    public static void Delete(LinkedList<int> myLList, int index)
-    {
-        if (myLList == null || index < 0)
-        {
-            return; // Return if the list is null or index is negative
-        }
+class LList{
+    public static void Delete(LinkedList<int> myLList, int index){
+        int count = 0;
+        LinkedListNode<int> current;
+        current = myLList.First;
 
-        // Handling deletion at the start of the list
-        if (index == 0)
-        {
-            if (myLList.First != null)
-            {
-                myLList.RemoveFirst();
+        while(current != null){
+            if(count == index){
+                myLList.Remove(current);
             }
-            return;
-        }
-
-        LinkedListNode<int> current = myLList.First;
-        int currentIndex = 0;
-
-        // Traverse the list to find the node at the given index
-        while (current != null && currentIndex < index)
-        {
             current = current.Next;
-            currentIndex++;
-        }
-
-        // If the node exists, remove it
-        if (current != null)
-        {
-            myLList.Remove(current);
+            count++;
         }
     }
 }
