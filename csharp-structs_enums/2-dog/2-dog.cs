961df5dc722bf394a0d2e8fb65200599c9e34bfb
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-02-04 11:53:16 +0000

    2

--- csharp-structs_enums/2-dog/2-dog.cs
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -1,3 +1,12 @@
+using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
 public struct Dog
 {
     public string name;
@@ -5,6 +14,7 @@ public struct Dog
     public string owner;
     public Rating rating;
 
+    // Constructor for the Dog struct
     public Dog(string name, float age, string owner, Rating rating)
     {
         this.name = name;
