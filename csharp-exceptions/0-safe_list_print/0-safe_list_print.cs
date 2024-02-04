a833fdcd92f1d9868ff805cd751b007df416c381
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-02-04 11:11:46 +0000

    0

--- csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -3,47 +3,21 @@ using System.Collections.Generic;
 
 public class List
 {
-    // Method to safely print elements of the list
     public static int SafePrint(List<int> myList, int n)
     {
-        int printedCount = 0;
-        try
+        int count = 0;
+        for (int i = 0; i < n; i++)
         {
-            
-            for (int i = 0; i < n; i++)
+            try
             {
                 Console.WriteLine(myList[i]);
-                printedCount++;
+                count++;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                break; 
             }
         }
-        catch (ArgumentOutOfRangeException)
-        {
-            // If n exceeds the list size, catch the exception
-            // No further action needed as we just stop printing
-        }
-        return printedCount;
-    }
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        List<int> myList = new List<int>() {1, 2, 3, 4, 5};
-        int count;
-
-        // Print all elements in the list
-        count = List.SafePrint(myList, myList.Count);
-        Console.WriteLine("Number of integers printed: " + count);
-        Console.WriteLine();
-
-        // Print fewer elements than the list contains
-        count = List.SafePrint(myList, myList.Count - 2);
-        Console.WriteLine("Number of integers printed: " + count);
-        Console.WriteLine();
-
-        // Attempt to print more elements than the list contains
-        count = List.SafePrint(myList, myList.Count + 2);
-        Console.WriteLine("Number of integers printed: " + count);
+        return count;
     }
 }
