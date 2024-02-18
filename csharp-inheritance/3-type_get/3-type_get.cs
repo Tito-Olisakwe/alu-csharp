48c6f4c28b6b8d85ecdc634c06be4d4999dac949
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-02-18 02:51:47 +0000

    3

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -5,29 +5,25 @@ using System.Collections.Generic;
 /// <summary>
 /// Provides functionality to print details of objects.
 /// </summary>
-public class Obj
+class Obj
 {
     /// <summary>
     /// Prints the names of the available properties and methods of an object.
     /// </summary>
     /// <param name="myObj">The object to inspect.</param>
-    public static void Print(object myObj){
-
-        TypeInfo t =  myObj.GetType().GetTypeInfo();
-        IEnumerable<PropertyInfo> pList = t.GetProperties();
-        Console.WriteLine($"{t.Name} Properties:");
-
-        foreach(PropertyInfo p in pList){
+    public static void Print(object myObj)
+    {
+        string cType = myObj.GetType().Name;
+        Type t = myObj.GetType();
+        Console.WriteLine("{0} Properties:", cType);
+        foreach (var p in t.GetProperties())
+        {
             Console.WriteLine(p.Name);
         }
-
-        IEnumerable<MethodInfo> pMethod = t.GetMethods();
-        Console.WriteLine($"{t.Name} Methods:");
-
-        foreach(MethodInfo m in pMethod){
-            if(m.DeclaringType == myObj.GetType()){
-                 Console.WriteLine(m.Name);
-            }
+        Console.WriteLine("{0} Methods:", cType);
+        foreach (var m in t.GetMethods())
+        {
+            Console.WriteLine(m.Name);
         }
     }
 }
