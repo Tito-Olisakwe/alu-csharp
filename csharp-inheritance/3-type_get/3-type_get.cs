b814a51e9258e3662ed2caa59b43f9c6c5c5a663
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-02-18 04:31:16 +0000

    3

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,6 +1,6 @@
 using System;
-using System.Reflection;
 using System.Collections.Generic;
+using System.Reflection;
 
     /// <summary>Represents the class Obj.</summary>
 
@@ -13,27 +13,17 @@ public class Obj
     public static void Print(object myObj)
     {
         Type type = myObj.GetType();
+        
         Console.WriteLine($"{type.Name} Properties:");
-        foreach (var prop in type.GetProperties())
+        foreach (PropertyInfo prop in type.GetProperties())
         {
             Console.WriteLine(prop.Name);
         }
-
+        
         Console.WriteLine($"{type.Name} Methods:");
-        var methods = type.GetMethods().Where(m => !m.IsSpecialName && m.DeclaringType == type);
-
-        // Example of a type-specific filter (conceptual, not fully implemented)
-        var filteredMethods = methods.Select(m => m.Name).Distinct();
-        if (type == typeof(String))
+        foreach (MethodInfo method in type.GetMethods())
         {
-            // Add or remove specific methods for the String type
-            filteredMethods = filteredMethods.Where(name => !name.StartsWith("get_") && !name.StartsWith("set_"));
-        }
-        // Additional type checks and filters could be added here
-
-        foreach (var methodName in filteredMethods.OrderBy(name => name))
-        {
-            Console.WriteLine(methodName);
+            Console.WriteLine(method.Name);
         }
     }
 }
