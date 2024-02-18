f78f573b192a32a47cf2c427398b4c889068e7b7
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-02-18 03:10:46 +0000

    4

--- csharp-inheritance/4-inherit/4-inherit.cs
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -9,19 +9,3 @@ public class Dog : Animal
 {
     // Empty class
 }
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Animal myAnimal = new Animal();
-        Dog myDog = new Dog();
-        object obj = myDog;
-        Animal anotherAnimal = myDog;
-
-        Console.WriteLine("myAnimal is a {0}", myAnimal.GetType());
-        Console.WriteLine("myDog is a {0}", myDog.GetType());
-        Console.WriteLine("obj is a {0}", obj.GetType());
-        Console.WriteLine("anotherAnimal is a {0}", anotherAnimal.GetType());
-    }
-}
