bd49d187682d7466426b8c741d14bde253dfca78
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-02-18 03:12:00 +0000

    4

--- csharp-inheritance/4-inherit/4-inherit.cs
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -1,10 +1,16 @@
 using System;
 
+/// <summary>
+/// Represents the most basic form of an animal.
+/// </summary>
 public class Animal
 {
     // Empty class
 }
 
+/// <summary>
+/// Represents a dog, which is a derivative of an animal.
+/// </summary>
 public class Dog : Animal
 {
     // Empty class
