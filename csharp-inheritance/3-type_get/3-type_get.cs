af5dc0e931c5db20188f09e313135cd17de2378f
Tito-Paris Olisakwe <113371193+Tito-Olisakwe@users.noreply.github.com>
2024-02-18 03:22:15 +0000

    3

--- csharp-inheritance/3-type_get/3-type_get.cs
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,35 +1,72 @@
+using System.Reflection;
 using System;
 using System.Collections.Generic;
-using System.Reflection;
 
-/// <summary>Represents the class Obj.</summary>
-class Obj
-{
-    /// <summary>
-    /// prints the names of the available properties and methods of an object.
-    /// </summary>
-    /// <param name="myObj">Object from where to get informations.</param>
-    public static void Print(object myObj)
-{
-    TypeInfo objTypeInfo = myObj.GetType().GetTypeInfo();
-    Console.WriteLine($"{objTypeInfo.Name} Properties:");
-    foreach (PropertyInfo prop in objTypeInfo.GetProperties())
-    {
-        Console.WriteLine(prop.Name);
-    }
 
-    Console.WriteLine($"{objTypeInfo.Name} Methods:");
-    var allowedMethods = new HashSet<string>
-    {
-        "CompareTo", "Equals", "GetHashCode", "ToString", "TryFormat",
-        "Parse", "TryParse", "GetTypeCode", "GetType"
-    };
-    foreach (MethodInfo method in objTypeInfo.GetMethods())
-    {
-        if (allowedMethods.Contains(method.Name))
-        {
-            Console.WriteLine(method.Name);
-        }
-    }
+/*
+TypeInfo t = typeof(Calendar).GetTypeInfo();
+IEnumerable<PropertyInfo> pList = t.DeclaredProperties;
+IEnumerable<MethodInfo> mList = t.DeclaredMethods;
+
+StringBuilder sb = new StringBuilder();
+
+sb.Append("Properties:");
+foreach (PropertyInfo p in pList)
+{
+
+    sb.Append("\n" + p.DeclaringType.Name + ": " + p.Name);
 }
+sb.Append("\nMethods:");
+foreach (MethodInfo m in mList)
+{
+    sb.Append("\n" + m.DeclaringType.Name + ": " + m.Name);
+}
+
+Console.WriteLine(sb.ToString());
+*/
+
+
+class Obj {
+
+
+    public static void Print(object myObj){
+
+        TypeInfo t =  myObj.GetType().GetTypeInfo();
+        // IEnumerable<PropertyInfo> pList  = t.DeclaredProperties;
+        IEnumerable<PropertyInfo> pList = t.GetProperties();
+        //Console.WriteLine(pList.First().DeclaringType.Name + " Properties:");
+        Console.WriteLine($"{t.Name} Properties:");
+ 
+        //StringBuilder sb = new StringBuilder();
+        //sb.Append(pList.First().DeclaringType.Name + " Properties:");
+
+
+
+        foreach(PropertyInfo p in pList){
+         //if(p.Attributes == None){
+
+          //  }
+            Console.WriteLine(p.Name);
+           // sb.Append("\n" + p.Name);
+        }
+
+        IEnumerable<MethodInfo> pMethod = t.GetMethods();
+        // Console.WriteLine(pMethod.First().DeclaringType.Name + " Methods:");
+        // sb.Append(pMethod.First().DeclaringType.Name + " Method:");
+        Console.WriteLine($"{t.Name} Methods:");
+
+        foreach(MethodInfo m in pMethod){
+            if(m.DeclaringType == myObj.GetType()){
+                 Console.WriteLine(m.Name);
+            }
+            // Console.WriteLine(m.Name);
+          
+           // sb.Append("\n" + m.Name);
+        }
+
+        // Console.WriteLine(sb.ToString());
+
+        
+
+    }
 }
