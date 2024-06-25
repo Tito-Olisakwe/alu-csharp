492870025b8a763c9430e454958ebfedde43fdaf
Tito-Olisakwe <t.olisakwe@alustudent.com>
2024-06-25 19:05:39 +0200

    0

--- csharp-generics/0-queue/queue.cs
+++ csharp-generics/0-queue/queue.cs
@@ -1,14 +1,17 @@
 using System;
 
 /// <summary>
-/// queue of type defined
+/// Queue of a specified type.
 /// </summary>
-/// <typeparam name="T"></typeparam>
-public class Queue<T>  {
-    
-    public Type CheckType(){
+/// <typeparam name="T">The type of elements in the queue.</typeparam>
+public class Queue<T>
+{
+    /// <summary>
+    /// Checks the type of elements in the queue.
+    /// </summary>
+    /// <returns>The type of elements in the queue.</returns>
+    public Type CheckType()
+    {
         return typeof(T);
     }
-
-    
 }
