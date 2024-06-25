51ee3ecccdcbb0cedd7178c468f234e88b39cb63
Tito-Olisakwe <t.olisakwe@alustudent.com>
2024-06-25 18:45:46 +0200

    0

---
+++ csharp-generics/0-queue/0-queue.cs
@@ -0,0 +1,14 @@
+using System;
+
+/// <summary>
+/// queue of type defined
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Queue<T>  {
+    
+    public Type CheckType(){
+        return typeof(T);
+    }
+
+    
+}
