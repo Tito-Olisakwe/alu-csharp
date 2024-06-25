30cfb2ce12d30c1b9b4a467c91d168a056c6f2ce
Tito-Olisakwe <t.olisakwe@alustudent.com>
2024-06-25 21:37:22 +0200

    0

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+using System;
+
+/// <summary>
+/// Base class providing foundational properties for inheritance.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Gets or sets the name associated with the instance.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Overrides the default ToString() method to display custom properties.
+    /// </summary>
+    /// <returns>A string representation of the object, including its name and type.</returns>
+    public override string ToString()
+    {
+        return $"{Name} is a {this.GetType()}";
+    }
+}
