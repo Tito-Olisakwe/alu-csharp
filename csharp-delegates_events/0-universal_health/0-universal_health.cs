729f37e3aecbad7097c77933a32ff6853b230cd7
Tito-Olisakwe <t.olisakwe@alustudent.com>
2024-06-25 23:59:46 +0200

    0

--- csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -25,7 +25,7 @@ public class Player
         }
 
         // Handle null or empty string for name
-        this.name = !string.IsNullOrEmpty(name) ? name : " ";
+        this.name = !string.IsNullOrEmpty(name) ? name : "";
         
         this.maxHp = maxHp;
         this.hp = maxHp;
