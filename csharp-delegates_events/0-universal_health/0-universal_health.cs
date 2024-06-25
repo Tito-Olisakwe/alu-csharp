4a6692f9e35e25ee330160f56401b3a2078b1a0c
Tito-Olisakwe <t.olisakwe@alustudent.com>
2024-06-25 23:47:01 +0200

    0

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,39 @@
+#nullable enable
+
+using System;
+
+/// <summary>
+/// Represents a player character with health properties and methods.
+/// </summary>
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <summary>
+    /// Initializes a new instance of the Player class with default values.
+    /// </summary>
+    /// <param name="name">The name of the player (default is "Player").</param>
+    /// <param name="maxHp">The maximum health points of the player (default is 100).</param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+
+        this.name = name ?? throw new ArgumentNullException(nameof(name), "Player name cannot be null.");
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    /// <summary>
+    /// Prints the current health status of the player.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
