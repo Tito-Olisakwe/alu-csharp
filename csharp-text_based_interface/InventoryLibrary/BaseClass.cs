19c7cd107a513271ee975384fcda9c99f4f6678d
Tito-Olisakwe <t.olisakwe@alustudent.com>
2024-07-01 11:13:29 +0200

    0

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    /// <summary>
+    /// Base class that all other classes inherit from
+    /// </summary>
+    public class BaseClass
+    {
+        /// <summary>
+        /// Unique identifier for the object
+        /// </summary>
+        public string id { get; set; }
+
+        /// <summary>
+        /// Date and time when the object was created
+        /// </summary>
+        public DateTime date_created { get; set; }
+
+        /// <summary>
+        /// Date and time when the object was last updated
+        /// </summary>
+        public DateTime date_updated { get; set; }
+
+        /// <summary>
+        /// Constructor to initialize the object
+        /// </summary>
+        public BaseClass()
+        {
+            this.id = Guid.NewGuid().ToString();
+            this.date_created = DateTime.Now;
+            this.date_updated = DateTime.Now;
+        }
+    }
+}
